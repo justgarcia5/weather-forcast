31058d7a63a88bdab49e1bc4e8291e24b9624515
JUSTGARCIA5 <JUSTGARCIA5@GMAIL.COM>
2021-11-23 10:58:35 -0800

    first commit

---
+++ src/components/FutureForcast.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react"
+
+export default function FutureForcast(props) {
+    const [fourDayForcast, setFourDayForcast] = useState(null);
+
+    useEffect(() => {
+            if(props.weather.daily)
+                setFourDayForcast(() => props.weather.daily.filter((day, index) => index < 5))
+    }, [props.weather.daily])
+
+    const unixConverter = (num) => {
+        let a = new Date(num * 1000);
+        return a.toString().slice(0,3);
+    }
+
+    const kelvinToFahrenheit = (num) => {
+        let fahrenheit = Math.ceil((num - 273.15) * 9) / 5 + 32;
+        return Math.ceil(fahrenheit);
+    }
+
+    return(
+        <div className='cards-div'>
+            {fourDayForcast &&
+                fourDayForcast.map((day, index) => {
+                    return(
+                        <div key={index} className='cards'>
+                            <h4>{unixConverter(day.dt)}</h4>
+                            <img src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} alt='weather icon' />
+                            <div className='temp-div'>
+                                <p>{kelvinToFahrenheit(day.temp.max) + '°'}</p>
+                                <p>{kelvinToFahrenheit(day.temp.min) + '°'}</p>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
---
+++ src/components/WeatherApi.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+import FutureForcast from './FutureForcast';
+
+export default function WeatherApi () {
+    const [weather, setWeather] = useState([]);
+    const API = 'a5cce44d5ea6f7812807aeaabd8bb7ac';
+    let long = '-118.39';
+    let lat = '34.02';
+
+    useEffect(() => {
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude={part}&appid=${API}`)
+        .then(res => res.json())
+        .then(data => setWeather(data))
+
+    },[lat, long])
+
+    return(
+        <div>
+            <FutureForcast
+                weather={weather}
+            />
+        </div>
+    )
+}
